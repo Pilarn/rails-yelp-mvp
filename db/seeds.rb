bbcc6a304e1cdaae5cb9d930efd8ac1f4eba7d51
sandra navarro <pilarnv@gmail.com>
2021-07-27 11:04:35 -0400

    avance en ejercicio

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name:"El Fuego", address:"Ochagavia 345", phone_number:90060080)
+Restaurant.create(name:"El Marino", address:"Pedro Montt 117", phone_number:900050070)
+Restaurant.create(name:"El Fideo", address:"Varas 285", phone_number:900040060)
+Restaurant.create(name:"El Verdura", address:"Benavente 711", phone_number:900030050)
